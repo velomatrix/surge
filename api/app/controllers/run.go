de858bec29cd4b5a7e1499331c9849a7f8769136
Tim Tischler <tischler@purplecoffeecup.com>
2014-11-26 12:05:37 -0800

    Changing the map of fake runs to an array of pointers to runs

--- api/app/controllers/run.go
+++ api/app/controllers/run.go
@@ -10,18 +10,18 @@ type Run struct {
 }
 
 // temporary data store
-var runs = map[int]models.Run{
-	0: models.Run{RunId: 0, Name: "Run 0"},
-	1: models.Run{RunId: 1, Name: "Run 1"},
-	2: models.Run{RunId: 2, Name: "Run 2"},
-	3: models.Run{RunId: 3, Name: "Run 3"},
-	4: models.Run{RunId: 4, Name: "Run 4"},
-	5: models.Run{RunId: 5, Name: "Run 5"},
+var runs = [6]*models.Run{
+	&models.Run{RunId: 0, Name: "Run 0"},
+	&models.Run{RunId: 1, Name: "Run 1"},
+	&models.Run{RunId: 2, Name: "Run 2"},
+	&models.Run{RunId: 3, Name: "Run 3"},
+	&models.Run{RunId: 4, Name: "Run 4"},
+	&models.Run{RunId: 5, Name: "Run 5"},
 }
 
 // returns a list of runs
 func (c Run) Index() revel.Result {
-	return c.RenderJson(runs[0])
+	return c.RenderJson(runs)
 }
 
 // returns a single run
