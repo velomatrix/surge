d329029752ba0cb8f29f6ae1e52e8413c092bbbb
Tim Tischler <tischler@purplecoffeecup.com>
2014-11-25 21:55:31 -0800

    Adding a new route to show an individual run.

--- api/app/controllers/run.go
+++ api/app/controllers/run.go
@@ -9,13 +9,22 @@ type Run struct {
 	*revel.Controller
 }
 
+// temporary data store
+var runs = map[int]models.Run{
+	0: models.Run{RunId: 0, Name: "Run 0"},
+	1: models.Run{RunId: 1, Name: "Run 1"},
+	2: models.Run{RunId: 2, Name: "Run 2"},
+	3: models.Run{RunId: 3, Name: "Run 3"},
+	4: models.Run{RunId: 4, Name: "Run 4"},
+	5: models.Run{RunId: 5, Name: "Run 5"},
+}
+
+// returns a list of runs
 func (c Run) Index() revel.Result {
+	return c.RenderJson(runs[0])
+}
 
-	//	runs := []*models.Run{
-	//		&models.Run{run_id: 0, name: "Big Load"},
-	//	}
-
-	run := models.Run{RunId: 0, Name: "Foo"}
-
-	return c.RenderJson(run)
+// returns a single run
+func (c Run) Show(id int) revel.Result {
+	return c.RenderJson(runs[id])
 }
